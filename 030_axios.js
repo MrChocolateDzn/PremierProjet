acce68efe6fc855cf7ba4ba94574d5216d7f6efa
vfleischmann <victor.fleischmann99@gmail.com>
2020-09-17 15:17:01 +0200

    Mon 1er commit

--- 030_axios.js
+++ 030_axios.js
@@ -1,6 +1,19 @@
 var app= new Vue({
     el: '#totoro',    
-    mounted(){
-        
+    mounted()
+    {
+        window.axios.get('https://pokeapi.co/api/v2/pokemon/1/').then(response =>
+        {
+            console.log(response.data);
+            console.log('Le nom du pokemon est : '+response.data.name);
+            this.statistiques = response.data.stats;
+            console.log(this.statistiques);
+            this.statistiques.forEach(unestatistique =>
+            {
+                console.log('Le nom est : '+unestatistique.stat.name);
+                console.log('Qui a pour valeur : '+unestatistique.base_stat);
+            });
+            console.log('La stat defense vaut : '+this.statistiques[2].base_stat);
+        });
     }
 });
